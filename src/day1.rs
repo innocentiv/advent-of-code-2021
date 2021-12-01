5b843b094774b87b506b75e83ea0a4b716b635df
Valerio Innocenti Sedili <valerio@brandbassador.com>
2021-12-01 14:08:59 +0100

    feat: first solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,87 +1,65 @@
-#[aoc(day1, part1, Bytes)]
-pub fn part1_bytes(input: &[u8]) -> i32 {
-    input.iter().fold(0, |sum, c| match c {
-        b'(' => sum + 1,
-        b')' => sum - 1,
-        _ => unreachable!(),
-    })
+use itertools::Itertools;
+
+#[aoc_generator(day1)]
+pub fn generator(input: &str) -> Vec<i32> {
+    input
+        .lines()
+        .map(|line| line.trim())
+        .map(|line| i32::from_str_radix(line, 10).unwrap_or(0))
+        .collect()
 }
 
-#[aoc(day1, part1, Chars)]
-pub fn part1_chars(input: &str) -> i32 {
-    input.chars().fold(0, |sum, c| match c {
-        '(' => sum + 1,
-        ')' => sum - 1,
-        _ => unreachable!(),
-    })
+#[aoc(day1, part1)]
+pub fn solver_part1(input: &Vec<i32>) -> u32 {
+    input.iter().tuple_windows().fold(
+        0,
+        |acc, (elem, next)| if elem < next { acc + 1 } else { acc },
+    )
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> usize {
-    let mut sum: u32 = 0;
-
-    for (i, c) in input.as_bytes().iter().enumerate() {
-        match c {
-            b'(' => sum += 1,
-            b')' => if let Some(s) = sum.checked_sub(1) {
-                sum = s;
-            } else {
-                return i + 1;
-            },
-            _ => unreachable!(),
-        }
-    }
-
-    unreachable!()
+pub fn solver_part2(input: &Vec<i32>) -> u32 {
+    input
+        .iter()
+        .tuple_windows()
+        .fold(0, |acc, (a, _b, _c, d)| if a < d { acc + 1 } else { acc })
 }
+
+// :: ---
 
 #[cfg(test)]
 mod tests {
-    use super::{part1_chars as part1, part2};
+    use super::*;
 
-    // (()) and ()() both result in floor 0.
     #[test]
-    fn sample1() {
-        assert_eq!(part1("(())"), 0);
-        assert_eq!(part1("()()"), 0);
+    fn example_1() {
+        let input = "199
+                     200
+                     208
+                     210
+                     200
+                     207
+                     240
+                     269
+                     260
+                     263";
+
+        assert_eq!(solver_part1(&generator(input)), 7);
     }
 
-    // ((( and (()(()( both result in floor 3.
     #[test]
-    fn sample2() {
-        assert_eq!(part1("((("), 3);
-        assert_eq!(part1("(()(()("), 3);
-    }
+    fn example_2() {
+        let input = "199
+                     200
+                     208
+                     210
+                     200
+                     207
+                     240
+                     269
+                     260
+                     263";
 
-    // ))((((( also results in floor 3.
-    #[test]
-    fn sample3() {
-        assert_eq!(part1("))((((("), 3);
-    }
-
-    // ()) and ))( both result in floor -1 (the first basement level).
-    #[test]
-    fn sample4() {
-        assert_eq!(part1("())"), -1);
-        assert_eq!(part1("))("), -1);
-    }
-
-    // ))) and )())()) both result in floor -3.
-    #[test]
-    fn sample5() {
-        assert_eq!(part1(")))"), -3);
-        assert_eq!(part1(")())())"), -3);
-    }
-
-    // ) causes him to enter the basement at character position 1.
-    #[test]
-    fn sample6() {
-        assert_eq!(part2(")"), 1);
-    }
-
-    // ()()) causes him to enter the basement at character position 5.
-    #[test]
-    fn sample7() {
-        assert_eq!(part2("()())"), 5);
+        assert_eq!(solver_part2(&generator(input)), 5);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,7 @@ extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
-extern crate crypto;
 
 pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
 
-aoc_lib!{ year = 2015 }
+aoc_lib! { year = 2021 }
